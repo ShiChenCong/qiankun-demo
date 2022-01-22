b2380008a934e634c751b539366d82a1ac917ad3
chencong <chencong@dian.so>
2022-01-22 18:23:20 +0800

    qiankun demo init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { registerMicroApps, start } from "qiankun";
 
+registerMicroApps([
+  {
+    name: "react app",
+    entry: "//localhost:3001",
+    container: "#container",
+    activeRule: "/app-react",
+  },
+  {
+    name: "vue app",
+    entry: "//localhost:8080",
+    container: "#container",
+    activeRule: "/app-vue",
+  },
+]);
+start();
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container"></div>
     </div>
   );
 }
